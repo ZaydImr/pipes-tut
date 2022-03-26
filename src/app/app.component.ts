8cdd64d48db838b64a5fde78ac9bdcbfef605042
zelimran <elimrani.z@gmail.com>
2022-03-26 20:53:28 +0100

    Finishing project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height: number = 0;
+
+  onNameChange(value: string){
+    this.name = value;
+  }
+
+  onDateChange(value: string){
+    this.date = value;
+  }
+
+  onAmountChange(value: string){
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(value: string){
+    this.height = parseFloat(value);
+  }
+
 }
